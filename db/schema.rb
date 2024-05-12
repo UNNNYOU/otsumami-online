0209500b95da3fa97f0d9efc087df2f794669694
UNNNYOU <unnnyou@gmail.com>
2024-05-12 22:58:26 +0900

    [add] userモデルの作成
    rails generate model コマンドを利用してuserモデルの作成を行いました。
    テーブルには以下の内容を追加しています。
    name string型　null:false
    外部キーとしてprefecture_idを指定して追加
    dbへの反映も行っています。

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_05_12_134120) do
+ActiveRecord::Schema[7.1].define(version: 2024_05_12_135430) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -20,4 +20,13 @@ ActiveRecord::Schema[7.1].define(version: 2024_05_12_134120) do
     t.datetime "updated_at", null: false
   end
 
+  create_table "users", force: :cascade do |t|
+    t.string "name", null: false
+    t.bigint "prefecture_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["prefecture_id"], name: "index_users_on_prefecture_id"
+  end
+
+  add_foreign_key "users", "prefectures"
 end
