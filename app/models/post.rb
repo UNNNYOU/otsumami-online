93b7acc9ae7fff18c06e260440265b6854bcf968
UNNNYOU <unnnyou@gmail.com>
2024-05-15 08:48:40 +0900

    [add] 新規投稿用の画面を作成しました。
    新規投稿用の画面を作成しました。
    それにあわせて、ルーティングの変更や関連付けなどを行いました。

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,4 +1,4 @@
 class Post < ApplicationRecord
   belongs_to :user
-  belongs_to :prefecture_id
+  belongs_to :prefecture_id, optional: true
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,9 @@
 Rails.application.routes.draw do
-  devise_for :user_authenticates, controllers: { registrations: 'user_authenticates/registrations' }
+  devise_for :user_authenticates, controllers: {registrations: "user_authenticates/registrations"}
 
-  get 'users/show'
-  root 'static_pages#top'
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+  root "static_pages#top"
 
-  # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
-  # Can be used by load balancers and uptime monitors to verify that the app is live.
-  get 'up' => 'rails/health#show', :as => :rails_health_check
-
-  # Defines the root path route ("/")
-  # root "posts#index"
+  resources :posts
+  get "users/show"
+  get "up" => "rails/health#show", :as => :rails_health_check
 end
