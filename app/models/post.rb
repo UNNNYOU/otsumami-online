bbe6a5be59987b0cbbafd566cfa7ff59aac14659
UNNNYOU <unnnyou@gmail.com>
2024-05-15 15:53:06 +0900

    [add] バリデーションの追加
    バリデーションの追加を行いました。
    以下の内容で追加しております。
    rating:　未記入を許可しない
    title: 未記入を許可しない, 最大文字数32文字
    body:  最大文字数256文字

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,4 +1,8 @@
 class Post < ApplicationRecord
   belongs_to :user
   belongs_to :prefecture_id, optional: true
+
+  validates :title, presence: true
+  validates :rating, presence: true, length: {maximum: 32}
+  validates :body, length: {maximum: 256}
 end
