ae30ec788c3e2b5b27fb76674d69d3b38f5bfbf4
UNNNYOU <unnnyou@gmail.com>
2024-05-16 20:42:11 +0900

    [add] 画像投稿時におけるvalidateの設定を行いました
    active_storage_validationsというgemを導入することで画像投稿時におけるvalidateの設定を行いました。
    現時点では最大４枚の画像投稿と、jpeg、jpg、pngの画像のみを投稿できるようにしています。

--- app/models/post.rb
+++ app/models/post.rb
@@ -3,8 +3,10 @@ class Post < ApplicationRecord
   belongs_to :prefecture, optional: true
 
   validates :rating, presence: true
-  validates :title, presence: true, length: { maximum: 10 }
-  validates :body, length: { maximum: 256 }
+  validates :title, presence: true, length: {maximum: 10}
+  validates :body, length: {maximum: 256}
 
   has_many_attached :images
+
+  validates :images, content_type: %i[png jpg jpeg], limit: {max: 4}
 end
