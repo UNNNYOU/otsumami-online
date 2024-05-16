48771a5aa603ad9b5d66fc1147b375dfb5799716
UNNNYOU <unnnyou@gmail.com>
2024-05-16 19:27:03 +0900

    [add] 画像投稿時にs3にて画像を保存する機能の実装
    画像投稿時にawsのs3にて画像の保存を行うよに処理を行いました。

--- app/models/post.rb
+++ app/models/post.rb
@@ -3,8 +3,8 @@ class Post < ApplicationRecord
   belongs_to :prefecture, optional: true
 
   validates :rating, presence: true
-  validates :title, presence: true, length: {maximum: 16}
-  validates :body, length: {maximum: 256}
+  validates :title, presence: true, length: { maximum: 10 }
+  validates :body, length: { maximum: 256 }
 
   has_many_attached :images
 end
