6df862051ee05566be96b0591beb1743b6457c0a
UNNNYOU <unnnyou@gmail.com>
2024-05-21 19:45:40 +0900

    [add] マイページ画面を作成しました。
    マイページ画面を作成しました。
    現時点では、自身の投稿したデータを表示すだけで具体的な機能の実装は行っておりません。

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,11 @@
 class UsersController < ApplicationController
   def show
+    @user = User.find(params[:id])
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:id)
   end
 end
